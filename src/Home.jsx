7fb4f8cb06dc1cacb74bb482376665217645f2b3
CarRoc07 <carlovhd@gmail.com>
2023-08-16 15:33:29 -0300

    first commit

---
+++ src/Home.jsx
@@ -0,0 +1,156 @@
+import { FaGithub, FaLinkedin, FaSearch } from 'react-icons/fa';
+import { WiRain, WiDaySunny, WiCloudy, WiSnow, WiHumidity, WiWindy, WiShowers, WiThunderstorm, WiFog, WiSmoke, WiDust, WiSandstorm, WiVolcano, WiStrongWind, WiTornado } from 'react-icons/wi';
+import { useEffect, useRef, useState } from 'react';
+import axios from 'axios'
+import {
+    Container,
+    GeneralTitle,
+    ContainerWeather,
+    ContainerSearch,
+    InputStyled,
+    ButtonStyled,
+    ErrorMessage,
+    ResultsContainer,
+    TextTemp,
+    TextCity,
+    TextCountry,
+    ContainerDetails,
+    ColDetails,
+    TextDetails,
+    ContainerFooter,
+    FooterText,
+    ContainerIcons,
+} from './home.styles';
+
+export const Home = () => {
+    const [data, setData] = useState({
+        celcius: 10,
+        name: "London",
+        humidity: 20,
+        speed: 2,
+        icon: "Clouds",
+        country: "GB"
+    })
+    const [error, setError] = useState(false)
+    const refInput = useRef(null)
+
+    const getData = async (city) => {
+        if(city === "") return;
+        try {
+            const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=02f91e5ad811802984345244dd1ceb48&units=metric`
+            const res = await axios.get(url)
+            console.log(res.data)
+            const data = await res.data;
+            console.log(data)
+            setData({
+                celcius: data.main.temp,
+                name: data.name,
+                humidity: data.main.humidity,
+                speed: data.wind.speed,
+                icon: data.weather[0].main,
+                country: data.sys.country
+            })
+            setError(false)
+            refInput.current.value = ""
+        } catch (error) {
+            setError(true)
+        }
+    }
+
+    const checkIconToRender = () => {
+        switch (data.icon) {
+            case 'Clear':
+                return <WiDaySunny size={190} />;
+            case 'Clouds':
+                return <WiCloudy size={190} />;
+            case 'Rain':
+                return <WiRain size={190} />;
+            case 'Drizzle':
+                return <WiShowers size={190} />;
+            case 'Thunderstorm':
+                return <WiThunderstorm size={190} />;
+            case 'Snow':
+                return <WiSnow size={190} />;
+            case 'Mist':
+                return <WiFog size={190} />;
+            case 'Fog':
+                return <WiFog size={190} />;
+            case 'Haze':
+                return <WiFog size={190} />;
+            case 'Smoke':
+                return <WiSmoke size={190} />;
+            case 'Dust':
+                return <WiDust size={190} />;
+            case 'Sand':
+                return <WiSandstorm size={190} />;
+            case 'Ash':
+                return <WiVolcano size={190} />;
+            case 'Squall':
+                return <WiStrongWind size={190} />;
+            case 'Tornado':
+                return <WiTornado size={190} />;
+            default:
+                return <WiTornado size={190} />;
+        }
+    }
+
+    useEffect(() => {
+        getData('london')
+    }, [])
+    
+    return (
+        <Container>
+            <GeneralTitle>
+                Live Weather
+            </GeneralTitle>
+            <ContainerWeather>
+                <ContainerSearch>
+                    <InputStyled type='text' placeholder='Search for a city' ref={refInput} error={error} autoFocus/>
+                    <ButtonStyled onClick={() => getData(refInput.current.value)}><FaSearch size={20}/></ButtonStyled>
+                </ContainerSearch>
+                {
+                    error ? <ErrorMessage>City not found</ErrorMessage> : null
+                }
+                <ResultsContainer>
+                    {
+                        checkIconToRender()
+                    }
+                    <TextTemp>
+                        {Math.round(data.celcius)} °C
+                    </TextTemp>
+                    <TextCity>
+                        {data.name}
+                    </TextCity>
+                    <TextCountry>
+                        {data.country}
+                    </TextCountry>
+                    <ContainerDetails>
+                        <ColDetails>
+                            <WiWindy size={120} />
+                            <div>
+                                <TextDetails>{Math.round(data.speed)} km/h</TextDetails>
+                                <TextDetails>Wind</TextDetails>
+                            </div>
+                        </ColDetails>
+                        <ColDetails>
+                            <WiHumidity size={120} />
+                            <div>
+                                <TextDetails>{Math.round(data.humidity)} %</TextDetails>
+                                <TextDetails>Humidity</TextDetails>
+                            </div>
+                        </ColDetails>
+                    </ContainerDetails>
+                </ResultsContainer> 
+            </ContainerWeather>
+            <ContainerFooter>
+                <FooterText>
+                    Desarollado por Carlos Stoll
+                </FooterText>
+                <ContainerIcons>
+                    <a href="https://github.com/CarRoc07" target='_blank' rel='noreferrer noopener'><FaGithub size={30} color='#ccc'/></a>
+                    <a href="https://www.linkedin.com/in/carlos-stoll-54242b221/" target='_blank' rel='noreferrer noopener'><FaLinkedin size={30} color='#ccc'/></a>
+                </ContainerIcons>
+            </ContainerFooter>
+        </Container>
+    )
+}
---
+++ src/home.styles.js
@@ -0,0 +1,127 @@
+import { styled, css } from "styled-components";
+
+export const Container = styled.div`
+    width: 100%;
+    height: 100vh;
+    background: url("https://res.cloudinary.com/dxkiqjvlp/image/upload/v1689721778/Eccomerce/32027_zshahq.jpg");
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    gap: 20px;
+    `
+
+export const GeneralTitle = styled.h1`
+    font-size: 60px;
+    font-weight: 700;
+    `
+
+export const ContainerWeather = styled.div`
+    width: 480px;
+    border: 2px solid white;
+    padding: 18px;
+    border-radius: 30px;
+    margin: 15px auto;
+    text-align: center;
+    background: linear-gradient(135deg, rgba(3,28,147,1) 0%, rgba(2,16,82,1) 100%);
+    `
+
+export const ContainerSearch = styled.div`
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+    `
+
+export const InputStyled = styled.input`
+    border: none;
+    outline: none;
+    padding: 12px 25px;
+    font-size: 22px;
+    border-radius: 30px;
+    color: black;
+    margin-right: 12px;
+    flex: 1;
+    background: #f3f1f1;
+
+    ${props =>
+    props.error &&
+    css`
+        border: 2px solid red;
+    `}
+    `
+
+export const ButtonStyled = styled.button`
+    border: 1px solid white;
+    outline: none;
+    width: 50px;
+    height: 50px;
+    border-radius: 50%;
+    cursor: pointer;
+    background: #2c2c2c;
+
+    &:hover {
+        background: black;
+    }
+    `
+
+export const ResultsContainer = styled.div`
+    margin: 0 auto;
+    `
+
+export const TextCity = styled.h2`
+    font-size: 50px;
+    font-weight: 400;
+    `
+
+export const TextCountry = styled.h3`
+    font-size: 22px;
+    `
+
+export const TextTemp = styled.h1`
+    font-size: 80px;
+    font-weight: 500;
+    `
+
+export const ContainerDetails = styled.div`
+    width: 100%;
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+    padding: 0px;
+    margin-top: 40px;
+    `
+
+export const ColDetails = styled.div`
+    display: flex;
+    align-items: center;
+    `
+
+export const TextDetails = styled.p`
+    font-size: 25px;
+    font-weight: 400;
+`
+
+export const FooterText = styled.p`
+    font-size: 18px;
+    opacity: 0.7;
+`
+
+export const ContainerFooter = styled.div`
+    display: flex;
+    align-items: center;
+    flex-direction: column;
+`
+
+export const ContainerIcons = styled.footer`
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    gap: 20px;
+`
+
+export const ErrorMessage = styled.p`
+    color: red;
+    text-align: left;
+    margin: 6px;
+    font-size: 18px;
+    font-weight: 500;
+    `
